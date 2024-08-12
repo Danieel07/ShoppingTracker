596aaeee3ecde787e99803e79d5e7c61039c1e4c
Daniel Nieto Llanos <dnietollanos@gmail.com>
2024-08-12 12:13:47 -0500

    Actualización de la vista

--- src/co/edu/unbosque/controller/Controller.java
+++ src/co/edu/unbosque/controller/Controller.java
@@ -1,13 +1,10 @@
 package co.edu.unbosque.controller;
 import co.edu.unbosque.model.ModelFacade;
-import co.edu.unbosque.view.ViewFacade;
 
 public class Controller {
 	private ModelFacade modelFacade;
-	private ViewFacade viewfacade;
 	
 	public Controller() {
-		viewfacade = new ViewFacade();
 		modelFacade = new ModelFacade();
 	}
 }
